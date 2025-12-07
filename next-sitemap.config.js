80164153531135304a9e08c8cfbf3b6af7e34e27
remotejobbay <barnwool@yahoo.com>
2025-12-07 19:02:18 +0000

    fix: add siteUrl to next-sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,1 +1,7 @@
-
+// next-sitemap.config.js
+module.exports = {
+  // Use your confirmed custom domain
+  siteUrl: 'https://www.remotejobbay.com', // or 'https://remotejobbay.com' if you prefer
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+}
